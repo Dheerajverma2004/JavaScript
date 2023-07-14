f7b20e8826298471d6ebe78f6a254103712361a5
Dheeraj verma <dheerajverma.cp@gmail.com>
2023-07-14 16:49:38 +0530

    initial commit

---
+++ Basic.js
@@ -0,0 +1,7 @@
+// printing in javaScript 
+console.log("D&A");
+console.log('D&A');
+// using variable , prompt() , and console.log() together
+var age = prompt("How old are you?");
+console.log(age);
+// using alert
