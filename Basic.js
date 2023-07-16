8e6523ee7088d6447453186e87cbaba45fc1acc1
Dheeraj verma <dheerajverma.cp@gmail.com>
2023-07-16 19:37:39 +0530

    added new js files

--- Basic.js
+++ Basic.js
@@ -1,7 +1,15 @@
-// printing in javaScript 
-console.log("D&A");
-console.log('D&A');
-// using variable , prompt() , and console.log() together
-var age = prompt("How old are you?");
-console.log(age);
-// using alert
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
---
+++ a.js
@@ -0,0 +1,29 @@
+//...JavaScript...
+
+//1.1 Printing in JavaScript
+//1.1.1 Browser console
+//1.1.2 printing string
+console.log("Dheeraj verma");
+console.log(4+10);
+
+const prompt = require("prompt-sync")();
+
+let myVar=prompt("what is the name of your laptop:");
+console.log(myVar);
+
+//1.2 Linking HTML with JavaScript
+"simple hai yaar web development ki starting me to yahi kiya tha"
+//1.2.1 Open your code editor and create an HTML file.
+//1.2.2 Within your HTML file, set up the HTML tags, doctype, HTML, head, and body, and then proceed and add the script tags.
+//1.2.3 Place some JavaScript code within the script tags. You can use console. log("hello world!")
+
+//1.3 Very Basic of Javascript 
+//1.3.1 Create a separate file called app with the extension .js.
+//1.3.2 Within the .js file, add some JavaScript code.
+//1.3.3 Link to the separate .js file within the HTML file you created in Practice exercise 1.2.
+//1.3.4 Open the HTML file within your browser and check to see whether the JavaScript code ran properly.
+
+//1.4 Prompt : takes input from the user
+let note=prompt("Hello! guys...");
+console.log(note);
+//1.4.1 
